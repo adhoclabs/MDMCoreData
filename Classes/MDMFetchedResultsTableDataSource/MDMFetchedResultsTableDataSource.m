54698a010ca17edf61748aa68ff623b08b5016aa
Will Carter <will@nodesnoop.com>
2015-09-17 14:12:14 -0700

    Workaround iOS 9 bug for FRC didChangeObject delegate call

--- Classes/MDMFetchedResultsTableDataSource/MDMFetchedResultsTableDataSource.m
+++ Classes/MDMFetchedResultsTableDataSource/MDMFetchedResultsTableDataSource.m
@@ -281,10 +281,13 @@ forRowAtIndexPath:(NSIndexPath *)indexPath {
                 // The bug presents itself when moving a row from a newly deleted section or to a newly inserted section
                 // This code should be removed once the bug has been fixed, it is tracked in OpenRadar
                 // http://openradar.appspot.com/17684030
-                [self.tableView deleteRowsAtIndexPaths:@[indexPath]
-                                      withRowAnimation:UITableViewRowAnimationFade];
-                [self.tableView insertRowsAtIndexPaths:@[newIndexPath]
-                                      withRowAnimation:UITableViewRowAnimationAutomatic];
+                
+                if (![indexPath isEqual:newIndexPath]) {
+                    [self.tableView deleteRowsAtIndexPaths:@[indexPath]
+                                          withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView insertRowsAtIndexPaths:@[newIndexPath]
+                                          withRowAnimation:UITableViewRowAnimationAutomatic];
+                }
             }
             
             break;
